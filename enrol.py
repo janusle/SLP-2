8d3b5007d9a3869c40027b0051cd9d7a89390288
janusle <janusle@gmail.com>
2012-05-08 00:14:50 +1000

    added writelines

--- enrol.py
+++ enrol.py
@@ -2,13 +2,14 @@
 
 def readlines(filename):
     lines = []
-    f = open(filename, 'r')
+    with open(filename, 'r') as f:
 
-    for line in f:
+       for line in f:
 
-        line = line.lstrip().rstrip("\n")
-        if len(line) != 0 and line[0] != '#':
-           lines.append(line)
+          line = line.lstrip().rstrip("\n")
+          if len(line) != 0 and line[0] != '#':
+              lines.append(line)
+
     return lines
 
 
@@ -25,12 +26,30 @@ def readtable(filename):
     return table
 
 
+def writelines(filename, lines):
 
-
+    result = 1
+    try:
+       f = open(filename, "w")
+       for line in lines:
+           f.write(line + "\n")
+    except:
+       result = 0
+    finally:
+       f.close()
+       return result
 
 
 if __name__ == '__main__':
-     lines = readtable( "test.txt" )
-     #lines = readlines( "test.txt" )
+
+     #lines = readtable( "test.txt" )
+     lines = readlines( "test.txt" )
      print lines
+     writelines( "test_clone.txt" , lines )
+
+     lines = readtable( "test_clone.txt" )
+     #lines = readlines( "test_clone.txt" )
+     print lines
+
+
 
