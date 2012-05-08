3a6eb3dbdfc752880d81ee9f58a3c65abb663921
janusle <janusle@gmail.com>
2012-05-08 23:34:34 +1000

    added function classses and classInfo

--- enrol.py
+++ enrol.py
@@ -6,7 +6,7 @@ def readlines(filename):
 
        for line in f:
 
-          line = line.lstrip().rstrip("\n")
+          line = line.rstrip("\n").strip()
           if len(line) != 0 and line[0] != '#':
               lines.append(line)
 
@@ -38,39 +38,66 @@ def writelines(filename, lines):
        f.close()
        return result
 
+import os
+
 
 class Enrol:
 
     def __addSubjects(self, tables):
        for row in tables:
-           #error checking
-           self.__subjects[row[0]] = row[1]
+           if len(row) == 2:
+               self.__subjects[row[0]] = {"name":  row[1], "class": [] }
+           else:
+              pass # throw excepiton
+
+    def __readtable( self, dirpath, filename ):
+       filename = os.path.join( dirpath, filename )
+       tables = readtable( filename )
+       return tables
+
+    def __addClasses(self, tables):
+       for row in tables:
+           if len(row) == 5:
+               if row[0] not in self.__subjects[row[1]]["class"]:#avoid duplicated item
+                  self.__subjects[row[1]]["class"].append( row[0] )
+               self.__classes[row[0]] = ( row[1], row[2], row[3], row[4] )
+           else:
+              pass # throw exception
+
 
 
     def __init__(self, directory):
-       import os
-       self.__subjects = {}
+       self.__subjects = {} # structure { code : { name:xxx, class: [] } }
+
+       self.__classes = {} # structure { class_code: ( subjectcode,time,venue,tutor, students ) }
+       self.__subject_class = {}
+
+       tables = self.__readtable( directory, "SUBJECTS" )
+       self.__addSubjects( tables )
+
+       tables = self.__readtable( directory, "CLASSES" )
+       self.__addClasses( tables )
 
        for dirpath,_,files in os.walk( directory ):
            for filename in files:
                if filename.find("SUBJECTS") != -1:
-                   filename = os.path.join( dirpath, filename )
-                   tables = readtable( filename )
-                   self.__addSubjects( tables )
+                   pass
+               elif filename.find("CLASSES") != -1:
+                   pass
 
     def subjects(self):
         return self.__subjects.keys()
 
 
     def subjectName(self, code):
-        return self.__subjects[code]
+        return self.__subjects[code]["name"]
 
 
-    def classes(self):
-        pass
+    def classes(self, subject_code):
+        return self.__subjects[subject_code]["class"]
 
-    def classInfo(self):
-        pass
+    def classInfo(self, class_code):
+        return self.__classes[class_code]
 
 
 
@@ -88,6 +115,6 @@ if __name__ == '__main__':
      '''
      e = Enrol('data')
      print e.subjects()
-     print e.subjectName("ddd")
-     print e.subjectName("bbb")
-     print e.subjectName("dfdfdfdfd")
+     print e.classes("ddd")
+     #print e.subjectName("dfdfdfdfd")
+     print e.classInfo( "class6" )
