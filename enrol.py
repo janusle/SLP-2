b7e357de0fc1f9634a4dc538afe85b2831ad561b
janusle <janusle@gmail.com>
2012-05-09 01:20:51 +1000

    regular commit

--- enrol.py
+++ enrol.py
@@ -39,44 +39,80 @@ def writelines(filename, lines):
        return result
 
 import os
-
+import sys
 
 class Enrol:
 
-    def __addSubjects(self, tables):
+
+    def _addSubjects(self, tables):
        for row in tables:
            if len(row) == 2:
                self.__subjects[row[0]] = {"name":  row[1], "class": [] }
            else:
               pass # throw excepiton
 
-    def __readtable( self, dirpath, filename ):
-       filename = os.path.join( dirpath, filename )
+
+    def _readfile( self, filename ):
+       try:
+         lines = readlines( filename )
+       except:
+         return []
+       else:
+         return lines
+
+
+    def _readtable( self, filename ):
+       filename = os.path.join( self._directory, filename )
        tables = readtable( filename )
        return tables
 
-    def __addClasses(self, tables):
+
+    def _getStudents(self, class_code):
+
+       filename = os.path.join( self._directory, class_code + ".roll" )
+       return self._readfile( filename )
+
+    def _addStudents(self, class_code, students ):
+       for student in studnets:
+           if student not in self.__students:
+               self.__students[ student ] =  [ class_code ]
+           else:
+               if class_code not in self.__students[ student ]:
+                 self.__students[ student ].append(class_code)
+
+
+    def _addClasses(self, tables):
        for row in tables:
            if len(row) == 5:
                if row[0] not in self.__subjects[row[1]]["class"]:#avoid duplicated item
                   self.__subjects[row[1]]["class"].append( row[0] )
-               self.__classes[row[0]] = ( row[1], row[2], row[3], row[4] )
+
+               # row[0] is class code
+               students = self._getStudents( row[0] )
+               self.__classes[row[0]] = [ row[1], row[2], row[3], row[4] ,\
+                                          students ]
+               _addStudents( row[0], students )
            else:
               pass # throw exception
 
 
-
     def __init__(self, directory):
+
+       self._directory = directory
+
        self.__subjects = {} # structure { code : { name:xxx, class: [] } }
 
        self.__classes = {} # structure { class_code: ( subjectcode,time,venue,tutor, students ) }
-       self.__subject_class = {}
 
-       tables = self.__readtable( directory, "SUBJECTS" )
-       self.__addSubjects( tables )
+       self.__students = {} # structure{ id: [ class_code ... ] }
 
-       tables = self.__readtable( directory, "CLASSES" )
-       self.__addClasses( tables )
+       tables = self._readtable( "SUBJECTS" )
+       self._addSubjects( tables )
+
+       tables = self._readtable( "CLASSES" )
+       self._addClasses( tables )
+
+
 
        for dirpath,_,files in os.walk( directory ):
            for filename in files:
@@ -84,6 +120,7 @@ class Enrol:
                    pass
                elif filename.find("CLASSES") != -1:
                    pass
+
 
     def subjects(self):
         return self.__subjects.keys()
@@ -96,8 +133,9 @@ class Enrol:
     def classes(self, subject_code):
         return self.__subjects[subject_code]["class"]
 
+
     def classInfo(self, class_code):
-        return self.__classes[class_code]
+        return tuple(self.__classes[class_code])
 
 
 
