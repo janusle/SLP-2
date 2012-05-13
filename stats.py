dfec73570c898a38fb4469321ddbb8bb621e83bb
janusle <janusle@gmail.com>
2012-05-13 19:24:59 +1000

    finished last requirement

--- stats.py
+++ stats.py
@@ -3,6 +3,11 @@
 import enrol
 import sys
 import os
+import getopt
+
+
+def usage():
+    sys.stderr.write("Usage: ./stats [--student] [student number]\n")
 
 
 def getDataDir():
@@ -14,10 +19,28 @@ def getDataDir():
     if not os.path.exists( directory ):
       sys.stderr.write("Cannot visit the directory specified, \
 the directory may not exists\n")
-      sys.stderr.write("Usage: ./stats [--student] [student number]\n")
+      usage()
       sys.exit(-1)
 
     return directory
+
+
+def getStudentId(argv):
+
+   try:
+          optlist, args = getopt.getopt( sys.argv[1:], "", ["student="] )
+   except getopt.GetoptError as err:
+          sys.stderr.write(str(err) + "\n")
+          usage()
+          sys.exit(-1)
+
+   student_id = None
+   for opt,val in optlist:
+        if opt == "--student":
+           student_id = val
+           break
+
+   return student_id
 
 
 
@@ -35,5 +58,18 @@ if len( sys.argv ) == 1: # no argument
    for subject in subjects:
        print "%s\t%s\tclasses: %d\tstudents: %d" % subject
 
+
 else:
-  pass
+   student_id = getStudentId( sys.argv[1:] )
+   classes = e.checkStudent( student_id )
+
+   if classes is not None:
+       subjects = [ (e.classInfo(klass)[0], e.subjectName( e.classInfo(klass)[0] ), \
+                     e.classInfo(klass)[1], e.classInfo(klass)[2] ) \
+                     for klass in classes ]
+
+       for subject in subjects:
+          print "%s\t%s\t%s @ %s" % subject
+
+   else:
+       print "The student didn't enrol any class"
